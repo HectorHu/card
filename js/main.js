a281d5685b0ec401c0a6a9b88d29721742433873
hhqcontinue <hhqcontinue@gmail.com>
2015-05-26 11:00:08 +0800

    增加切换图片淡入淡出效果

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 (function() {
-	var start_header = document.getElementById('start-header'),
+	var $_id = function (id) {
+		return typeof id === "string" ? document.getElementById(id) : id;
+	};
+	
+	var start_link = $_id('start-link'),
+		start_header = $_id('start-header'),
 		start_header_li = start_header.getElementsByTagName("li"),
 		i = 0;
 	var s = setInterval( function() {
 		start_header_li[i].style.display = 'inline-block';
 		if (++i == start_header_li.length) {
 			clearInterval(s);
+			start_link.href = './page.html';
 		}
 	}, 700);
-
-	var start_btn = document.getElementById('start-btn');
 })();
--- js/page.js
+++ js/page.js
@@ -2,9 +2,48 @@
 * @Author: Hector
 * @Date:   2015-05-22 17:15:52
 * @Last Modified by:   huhuaquan
-* @Last Modified time: 2015-05-25 19:59:13
+* @Last Modified time: 2015-05-26 10:58:41
 */
 (function() {
+	var $_id = function(id) {
+		return typeof id === 'string' ? document.getElementById(id) : id;
+	};
+
+	//设置透明度
+    function setOpacity(item,level){ 
+        if(item.filters){ 
+            item.style.filter = "alpha(opacity="+level+")";
+        }else{
+            item.style.opacity = level / 100;
+        }
+    }
+
+	//淡入处理函数
+    function fadeIn(item) {
+        setOpacity(item, 0);
+        for(var i = 0; i<=20; i++) {
+            (function(i){ 
+                var level = i * 5;
+                setTimeout(function(){ 
+                    setOpacity(item, level)
+                },i*25);
+            })(i);
+        }
+    }
+
+    //淡出处理函数
+    function fadeOut(item) {
+        setOpacity(item, 0);
+        for(var i = 0; i<=20; i++) {
+            (function(i){ 
+                var level = 100 - i * 5;
+                setTimeout(function(){ 
+                    setOpacity(item, level)
+                },i*25);
+            })(i);
+        }
+    }
+
 	function copyProperty(newDiv, old) {
 		newDiv.style.offsetLeft = old.style.offsetLeft;
 		newDiv.style.width = old.style.width;
@@ -12,21 +51,23 @@
 		newDiv.style.backgroundImage = old.style.backgroundImage;
 	}
 
-	var egg = document.getElementById('egg');
+	var egg = $_id('egg');
 	egg.addEventListener('webkitAnimationStart', listener, false);
 	egg.addEventListener('webkitAnimationEnd', listener, false);
 	egg.addEventListener('webkitAnimationIteration', listener, false);
 
 	function listener(e) {
 		if (this.offsetLeft == 100) {
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/egg_broke.jpg)";
+			fadeIn(this);
 			setTimeout(function () {growToKid()}, 500);
 		}
 	}
 
 	function growToKid() {	
 		egg.setAttribute('id', 'kid');
-		var kid = document.getElementById('kid');
+		var kid = $_id('kid');
 		kid.addEventListener('webkitAnimationStart', listenerKid, false);
 		kid.addEventListener('webkitAnimationEnd', listenerKid, false);
 		kid.addEventListener('webkitAnimationIteration', listenerKid, false);
@@ -36,13 +77,15 @@
 		if (this.offsetLeft == 280) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/kid.png)";
+			fadeIn(this);
 			setTimeout(function () {growToPupils()}, 500);
 		}
 	}
 
 	function growToPupils() {
-		var kid = document.getElementById('kid'),
+		var kid = $_id('kid'),
 			newDiv = document.createElement('div');
 		newDiv.id = 'pupils';
 		copyProperty(newDiv, kid);
@@ -57,13 +100,15 @@
 		if (this.offsetLeft == 460) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/pupils.png)";
+			fadeIn(this);
 			setTimeout(function () {growToJunior()}, 500);
 		}
 	}
 
 	function growToJunior() {
-		var pupils = document.getElementById('pupils'),
+		var pupils = $_id('pupils'),
 			newDiv = document.createElement('div');
 		newDiv.id = 'junior';
 		copyProperty(newDiv, pupils);
@@ -78,13 +123,15 @@
 		if (this.offsetLeft == 640) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/junior.jpg)";
+			fadeIn(this);
 			setTimeout(function () {growToSenior()}, 500);
 		}
 	}
 
 	function growToSenior() {
-		var junior = document.getElementById('junior'),
+		var junior = $_id('junior'),
 			newDiv = document.createElement('div');
 		newDiv.id = 'senior';
 		copyProperty(newDiv, junior);
@@ -99,13 +146,15 @@
 		if (this.offsetLeft == 820) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/senior.jpg)";
+			fadeIn(this);
 			setTimeout(function () {growToCollege()}, 500);
 		}
 	}
 
 	function growToCollege() {
-		var senior = document.getElementById('senior'),
+		var senior = $_id('senior'),
 			newDiv = document.createElement('div');
 		newDiv.id = 'college';
 		copyProperty(newDiv, senior);
@@ -120,13 +169,15 @@
 		if (this.offsetLeft == 1000) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/college.jpg)";
+			fadeIn(this);
 			setTimeout(function () {growToRecent()}, 500);
 		}
 	}
 
 	function growToRecent() {
-		var college = document.getElementById('college'),
+		var college = $_id('college'),
 			newDiv = document.createElement('div');
 		newDiv.id = 'recent';
 		copyProperty(newDiv, college);
@@ -141,7 +192,9 @@
 		if (this.offsetLeft == 1180) {
 			this.style.width = "72px";
 			this.style.height = "107px";
+			fadeOut(this);
 			this.style.backgroundImage = "url(./images/now.jpg)";
+			fadeIn(this);
 		}
 	}
 })();
